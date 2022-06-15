00c4b9027132bd2aeea27d1cc873eed54d31d4ec
GodStarLord <62302642+GodStarLord@users.noreply.github.com>
2022-06-16 00:48:12 +0530

    Outputting Dynamic Data & Working with Expressions in JSX
    - Use { JS_Variable_Name } notation in the HTML to render javascript variable or execute function in the Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022, 1, 14);
+    const expenseTitle = 'Car Insurance';
+    const expensePrice = 21.30;
+
   return (
     <div className="expense-item">
-      <div>May 20th, 2021</div>
+      <div>{expenseDate.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">$250</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expensePrice}</div>
       </div>
     </div>
   );
