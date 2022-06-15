c92aa445678e0f9acd8dc7f8597fadf021f0123c
GodStarLord <62302642+GodStarLord@users.noreply.github.com>
2022-06-15 15:58:28 +0530

    Building a First Custom Component
    - Create New Component ExpenseItem.js and add some basic HMTL elements to it.

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>May 20th, 2021</div>
+      <div>
+        <h2>Title</h2>
+        <div>$250</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
