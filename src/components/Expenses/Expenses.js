cd7065d6f991dcaf30430624bcd2d0987dcf89e9
GodStarLord <62302642+GodStarLord@users.noreply.github.com>
2022-06-16 16:52:56 +0530

    Time to Practice: Working with Events & State
    - Create Expense Filter Component and Lifting the State Up
    - Just logging the selected values

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,22 @@
+import React, { useState } from "react";
+
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedFilteredYear) => {
+    setFilteredYear(selectedFilteredYear);
+  };
+
   return (
     <div>
       <Card className="expenses">
+        <ExpensesFilter selectedYear={filteredYear} onFilterChange={filterChangeHandler} />
         <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
