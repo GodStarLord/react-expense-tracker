a4a38bc9a57bcf1482e59c0019ecaa632aa86c2b
GodStarLord <62302642+GodStarLord@users.noreply.github.com>
2022-06-16 14:00:34 +0530

    Adding Two-Way Binding
    - Upon Submission, Clear the Form Values
    - Setting the form control values from the js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -80,6 +80,11 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+
+    // Clear the form controls
+    setEnteredAmount("");
+    setEnteredDate("");
+    setEnteredTitle("");
   };
 
   return (
@@ -87,7 +92,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -95,6 +104,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -104,6 +114,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
